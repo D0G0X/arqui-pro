ed778c3a7d3a0da5afda34dccf36f24239766ea8
D0G0X <derlis2361@gmail.com>
2025-10-30 16:17:52 -0500

    un error pequeño xddd

--- backend/wedsocket/src/main.ts
+++ backend/wedsocket/src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+  const port = process.env.PORT ? Number(process.env.PORT) : 3006;
   await app.listen(port);
   console.log(`wedsocket server listening on ${port}`);
 }
